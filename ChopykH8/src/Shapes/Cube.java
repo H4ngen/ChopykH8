9216931b001f5f8ae891211a284d55f809418799
H4ngen <118844107+H4ngen@users.noreply.github.com>
2023-05-03 17:46:22 +0200

    Add files via upload

---
+++ ChopykH8/src/Shapes/Cube.java
@@ -0,0 +1,19 @@
+package Shapes;
+
+public class Cube extends Shape {
+    private final String name;
+
+    public Cube() {
+        this.name = "Cone";
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "This is a " + this.name;
+    }
+}
---
+++ ChopykH8/src/Shapes/Pentagon.java
@@ -0,0 +1,19 @@
+package Shapes;
+
+public class Pentagon extends Shape {
+    private final String name;
+
+    public Pentagon() {
+        this.name = "Pentagon";
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "This Shape is a " + this.name;
+    }
+}
